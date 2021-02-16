d2b9f8498b9ec6f7a5e25bf951dc732865cef988
Lena Hou <lenahou2013@gmail.com>
2021-02-16 23:26:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,7 @@ def song_count
   Song.all.select { |song| song.artist == self }
 end
 
-def add_song_by_name(name)
+def add_song_by_name(name, artist)
   song = Song.new(name, artist)
   add_song(song)
 end
