7e0f96a9d147c86b762756b458adde71216fd720
Lena Hou <lenahou2013@gmail.com>
2021-02-17 00:26:01 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,7 +5,21 @@ def initialize(name)
   @name = name
 end
 
+def posts
+  Post.all
+end
 
+def add_song(song)
+  song.artist = self
+end
 
+def add_post_by_name(name)
+  post = Post.new(name)
+  add_post(post)
+end
+
+def self.post_count
+  Post.all.count
+end
 
 end
